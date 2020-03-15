49de6dd91f04982a38a6f7ef3fa7de71ba031aa2
oguzhannsahinn <oguzhansahin95@outlook.com>
2020-03-15 16:44:02 +0300

    next 5 days done

--- src/App5days.vue
+++ src/App5days.vue
@@ -1,28 +1,38 @@
 <template>
-<div id="app" :class="typeof day1.main != 'undefined'">
+<div id="app" :class="typeof weather.main != 'undefined'">
     <main>
-
-        <div @keypress="funcs" class="search-box" :class="firstWeatherStatus">
+        <div @keypress="funcs" class="search-box">
             <input type="text" class="search-bar" placeholder="Search..." v-model="query" />
         </div>
 
-        <div class="weather-part" v-for="item of day1" v-bind:key="item.id">
-            <div class="weather-wrap" v-if="typeof item.main != 'undefined'" :class="item.weather[0].main">
+        <!-- input group -->
+        <div id="inputGroup" style="padding:15px 0; text-align:center" v-if="showDays">
+            <label v-for="(day, index) in daysforLabels" :key="index">
+                <span> {{day.text}} </span>
+                <input class="day-button" type="radio" v-model="currentList" v-bind:value="day.value">
+            </label>
+        </div>
 
-                <div class="weather-box">
-                    <div class="date" style="display:block; opacity:.75">
-                        {{formatter(item.dt_txt)}} {{formatter2(item.dt_txt)}}
+        <div>
+            <div class="weather-part" v-for="item of items" v-bind:key="item.id" v-on:click="displayToggle()">
+                <div class="weather-wrap" v-if="typeof item.main != 'undefined'" :class="item.weather[0].main">
+                    <div class="weather-box">
+                        <div class="date">
+                            <span> {{dateFormatter(item.dt_txt)}} </span>
+                            <span> {{timeFormatter(item.dt_txt)}} </span>
+                        </div>
+                        <div class="temp">{{ Math.round( item.main.temp )}}°C</div>
+                        <div class="weather">{{item.weather[0].main}}</div>
+                        <img src="./assets/sun.png" alt="clear" v-if="item.weather[0].main == 'Clear'" class="weather-image">
+                        <img src="./assets/cloud.png" alt="clouds" v-if="item.weather[0].main == 'Clouds'" class="weather-image">
+                        <img src="./assets/rain.png" alt="rain" v-if="item.weather[0].main == 'Rain'" class="weather-image">
+                        <img src="./assets/snow.png" alt="snow" v-if="item.weather[0].main == 'Snow'" class="weather-image">
                     </div>
-                    <div class="temp">{{ Math.round( item.main.temp )}}°C</div>
-                    <div class="weather">{{item.weather[0].main}}</div>
-                    <img src="./assets/sun.png" alt="sun" v-if="item.weather[0].main == 'Clear'" class="weather-image">
-                    <img src="./assets/cloud.png" alt="sun" v-if="item.weather[0].main == 'Clouds'" class="weather-image">
-                    <img src="./assets/rain.png" alt="sun" v-if="item.weather[0].main == 'Rain'" class="weather-image">
-                    <img src="./assets/snow.png" alt="cloud" v-if="item.weather[0].main == 'Snow'" class="weather-image">
                 </div>
-
             </div>
         </div>
+
+        <!-- </div> -->
     </main>
 </div>
 </template>
@@ -40,23 +50,55 @@ export default {
             date: '',
             currentDay: '',
             firstWeatherStatus: '',
-            day: {
-                0: 'Sunday',
-                1: 'Monday',
-                2: 'Tuesday',
-                3: 'Wednesday',
-                4: 'Thursday',
-                5: 'Friday',
-                6: 'Saturday'
+            showDays: false,
+            daysOfWeek: ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"],
+            next5days: [],
+            currentDayIndex: 0,
+            daysforLabels: [{
+                    text: "Today",
+                    value: "day1"
+                },
+                {
+                    text: "2. Day",
+                    value: "day2"
+                },
+                {
+                    text: "3. Day",
+                    value: "day3"
+                },
+                {
+                    text: "4. Day",
+                    value: "day4"
+                },
+                {
+                    text: "5. Day",
+                    value: "day5"
+                }
+            ],
+            day1Count: 1,
+            timestamp: '',
+            detailWeather: true,
+            whichDay: '',
+            counter: 0,
+            lists: {
+                day1: {},
+                day2: {},
+                day3: {},
+                day4: {},
+                day5: {}
             },
-            
-            day1: {},
-            remainingDays: {},
-            day1Count : 1,
-            remainingDayCount: 1
+            currentList: 'day1'
+        }
+    },
+    computed: {
+        items() {
+            return this.lists[this.currentList]
         }
     },
     methods: {
+        setCurrentList(list) {
+            this.currentList = list
+        },
         fetchWeather() {
             // fetch(`${this.url_base}weather?q=${this.query}&units=metric&APPID=${this.api_key}`)
             fetch(`${this.url_base}forecast?q=${this.query}&units=metric&APPID=${this.api_key}`)
@@ -81,44 +123,158 @@ export default {
 
                 setTimeout(() => {
                     this.daySplitter();
+                    this.getTodayName();
+                    this.showDays = true;
+                    // this.setCurrentList();
 
                 }, 650);
             }
 
-            setInterval(function(){
-                if(document.getElementsByClassName('weather-part').length > 1) {
-                    document.querySelectorAll('.weather-part')[0].style.display = 'block';
-                } 
+            setInterval(function () {
+                if (document.getElementsByClassName('weather-part').length > 1) {
+
+                    let styleObj = document.querySelectorAll('.weather-part')[0].style;
+                    styleObj.visibility = 'visible'
+                    styleObj.opacity = '1';
+                }
             }, 1000); // check after 100ms every time
         },
-        formatter(a) {
+
+        //get today's name
+        getTodayName() {
+            let todayString = new Date().toLocaleString('en-us', {
+                weekday: 'long'
+            });
+            this.timestamp = todayString;
+
+            //günleri sıralama ** this.timestamp = "saturday"
+            for (let i in this.daysOfWeek) {
+
+                if (this.daysOfWeek[i] == this.timestamp.slice(0, 3)) {
+                    this.currentDayIndex = i;
+                    this.daysforLabels[0].text = this.daysOfWeek[i]
+                }
+            }
+
+            //günler için sıralı dizi oluşturma. Today'den itibaren
+
+            let dayArr = [];
+            let firstTurnCount = 7;
+            let secondTurnCount = 5;
+            this.currentDayIndex = parseInt(this.currentDayIndex);
+            if(this.currentDayIndex < 3) {
+
+                firstTurnCount = (this.daysOfWeek.length - this.currentDayIndex);
+
+            }else {
+                secondTurnCount = 5 - firstTurnCount;
+            }
+                
+            if (this.currentDayIndex > 3) {
+
+                dayArr.push(this.daysOfWeek[this.currentDayIndex]);
+
+                for (let j = 0; j < secondTurnCount; j++) {
+
+                    dayArr.push(this.daysOfWeek[j])
+                }
+
+                this.daysOfWeek = dayArr;
+            } else {
+
+                for (let j = this.currentDayIndex; j < secondTurnCount; j++) {
+
+                    dayArr.push(this.daysOfWeek[j]);
+                }
+            }
+
+            let _daysforLabels = [];
+
+            this.next5days = dayArr;
+
+            this.daysforLabels.forEach((item, index) => {
+
+                _daysforLabels.push({
+                    text: this.next5days[index],
+                    value: item.value
+                });
+
+            });
+            this.daysforLabels = _daysforLabels
+
+        },
+        dateFormatter(a) {
             return a.split(" ")[0]
         },
-        formatter2(a) {
+        timeFormatter(a) {
             return a.split(" ")[1].split(":")[0] + ':00'
         },
+        //splitting all days by today and other days
         daySplitter() {
 
             let vm = this;
 
             for (let i = 0; i < vm.weather.length; i++) {
-                if (parseFloat(vm.weather[i].dt_txt.split("-")[2]) == vm.currentDay) {                    
+                if (parseFloat(vm.weather[i].dt_txt.split("-")[2]) == vm.currentDay) {
                     this.day1Count++;
-                }else {
-                    this.remainingDayCount++
                 }
             }
 
-            this.day1 = this.weather.slice(0,this.day1Count)
-            this.remainingDays = this.weather.slice(this.day1Count,this.weather.length)
+            this.lists.day1 = this.weather.slice(0, this.day1Count)
+            this.lists.day2 = this.weather.slice(this.day1Count, this.day1Count + 8)
+            this.lists.day3 = this.weather.slice(this.day1Count + 8, this.day1Count + 16)
+            this.lists.day4 = this.weather.slice(this.day1Count + 16, this.day1Count + 24)
+            this.lists.day5 = this.weather.slice(this.day1Count + 24, this.day1Count + 32)
 
         },
-    }
+        displayToggle() {
+            let el = document.querySelectorAll('.weather-part');
+
+            if (this.detailWeather) {
+                for (let i = 0; i < el.length; i++) {
+                    el[i].style.visibility = 'visible';
+                    el[i].style.opacity = '1';
+                }
+                this.detailWeather = !this.detailWeather;
+            } else {
+                for (let i = 1; i < el.length; i++) {
+                    el[i].style.visibility = 'hidden';
+                    el[i].style.opacity = '0';
+                }
+                this.detailWeather = !this.detailWeather;
+            }
+        }
+    },
 }
 </script>
 
 <style>
 @import url('https://fonts.googleapis.com/css?family=Quicksand&display=swap');
+
+label {
+    margin: 0 10px;
+    box-shadow: 0 5px 5px 0 rgba(150, 150, 150, 0.5);
+    padding: 5px;
+    border-radius: 4px;
+    color: #3d3d3d;
+    display: inline-grid;
+    height: 55px;
+}
+
+#inputGroup label {
+    font-size: 15px;
+}
+
+.day-button {
+    width: 40px;
+    position: relative;
+    height: 20px;
+    opacity: 1;
+}
+
+#inputGroup {
+    margin: 10px 0;
+}
 
 * {
     margin: 0;
@@ -132,10 +288,12 @@ export default {
     /* background-image: url('./assets/cold.jpg'); */
     background-size: cover;
     background-position: bottom;
-    transition: 0.4s;
+    transition: opacity 1.5s ease-in-out;
     border-bottom: 1px solid rgba(204, 204, 204, 0.233);
 
-    display:none;
+    visibility: hidden;
+    opacity: 0;
+    transition: visibility 0s, opacity 0.5s linear;
 }
 
 .weather-wrap.warm {
@@ -163,10 +321,11 @@ main {
     height: 100vh;
     background-image: linear-gradient(to bottom, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.25));
     overflow-y: auto;
-    background-color: #606060;
+    background-color: #fff;
 }
 
 .search-box {
+    background-color: transparent;
     width: 100%;
     padding: 25px;
 }
@@ -200,7 +359,7 @@ main {
 
 .weather-image {
     background-color: transparent;
-    margin: 0 0 0 20px;
+    margin: 0 0 0 10px;
     width: 50px;
     height: 50px;
     position: relative;
@@ -210,16 +369,23 @@ main {
 
 .weather-box .date {
     color: #fff;
-    font-size: 20px;
-    opacity: 1;
+    font-size: 16px;
+    top: 5px;
     text-align: center;
     text-shadow: 1px 3px rgba(0, 0, 0, 0.25);
-    padding-top: 10px;
+    display: inline-block;
+    position: relative;
+    opacity: .75;
+    height: auto;
+}
+
+.weather-box .date span {
+    display: block;
 }
 
 .weather-box .temp {
     display: inline-block;
-    padding: 10px 25px;
+    padding: 5px 25px;
     color: #fff;
     font-size: 30px;
     font-weight: 900;
@@ -227,13 +393,13 @@ main {
     text-shadow: 3px 3px rgba(0, 0, 0, 0.25);
     background-color: rgba(255, 255, 255, 0.25);
     border-radius: 15px;
-    margin: 30px 15px;
+    margin: 20px 15px;
     box-shadow: 3px 6px rgba(0, 0, 0, 0.25);
 }
 
 .weather-box .weather {
     color: #fff;
-    font-size: 22px;
+    font-size: 18px;
     font-style: italic;
     text-shadow: 3px 3px rgba(0, 0, 0, 0.25);
     display: inline-block;
--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
 import Vue from 'vue'
 // import App from './App.vue'
-import App5Days from './App5days.vue'
+import App5days from './App5days.vue'
 
 Vue.config.productionTip = false
 
 new Vue({
   // render: h => h(App),
-  render: h => h(App5Days),
+  render: h => h(App5days),
 }).$mount('#app')
